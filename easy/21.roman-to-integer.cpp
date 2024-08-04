6b1389a589f172c4dd87b19ca7957af3ba9c92b3
Md.Ashraful Haque <ashrafulhaque@users.noreply.github.com>
2024-08-04 22:22:59 +0600

    Problem Solved: 21.roman-to-integer

---
+++ easy/21.roman-to-integer.cpp
@@ -0,0 +1,30 @@
+// Problem: https://leetcode.com/problems/roman-to-integer/
+// Programmer: Md. Ashraful Haque
+// Date: 04.08.2024
+
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m;
+        
+        m['I'] = 1;
+        m['V'] = 5;
+        m['X'] = 10;
+        m['L'] = 50;
+        m['C'] = 100;
+        m['D'] = 500;
+        m['M'] = 1000;
+        
+        int result = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(m[s[i]] < m[s[i+1]]){
+                result -= m[s[i]];
+            }
+            else{
+                result += m[s[i]];
+            }
+        }
+        return result;
+    }
+};
